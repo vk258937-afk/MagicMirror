4700279b0af209e18bd0e61deb9ec71358da4485
vk258937-afk <vk258937@gmail.com>
2025-08-01 14:13:34 +0200

    Update config.js

--- config/config.js
+++ config/config.js
@@ -21,7 +21,38 @@ let config = {
 			dateFormat: "ddd, ll",
 			timezone: "America/Chicago"
 		}
+    },
+	{
+    module: "WallberryTheme",
+    position: "fullscreen_below", // Required Position
+    config: {
+      unsplashAccessKey: "Your Unsplash API Key", // REQUIRED
+      collections: "2589108" // optional - leave empty for a random photo
     }
+  },
+  // WB-clock adds local time (Optional Module)
+  {
+    module: "WallberryTheme/WB-clock",
+    position: "top_bar", // highly suggest using top_bar position
+    config: {
+      localCityName: "Seattle", // optional
+      otherCities: [
+      	{name: "DC", timezone: "US/Eastern"}, // optional
+      	{name: "Anchorage", timezone: "US/Alaska"} // optional
+      ]
+    }
+  },
+  // WB-weather adds weather (Optional Module)
+  {
+    module: "WallberryTheme/WB-weather",
+    position: "bottom_bar",  // Highly suggested location
+    config: {
+      // See "Configuration options" for more information.
+      apiKey: "Your openweathermap API key", // REQUIRED
+      latitude:   47.603230, // REQUIRED
+      longitude: -122.330276 // REQUIRED
+    }
+  }
   ]
 };
 
